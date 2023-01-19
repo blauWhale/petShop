3088e0511970ab5907bc97ab58a14d23faeddef8
zblaar <raphael.blaauw@bbcag.ch>
2023-01-19 16:26:57 +0100

    typo II

--- pet-shop-medusa-server/medusa-config.js
+++ pet-shop-medusa-server/medusa-config.js
@@ -23,7 +23,7 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001, https://legendary-caramel-4107d1.netlify.app";
+const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,https://legendary-caramel-4107d1.netlify.app";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000,https://petshop-frontend-production.up.railway.app";
